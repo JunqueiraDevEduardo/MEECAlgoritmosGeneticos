fb9a6140fea2d7b977e369f5b67716b5f57e0356
Eduardo Miguel Moreira Junqueira <junqueiradeveduardo@gmail.com>
2025-11-07 18:03:00 +0000

    Ex2 Toolbox com roda da roleta a funcionar e ainda exercício 1 terá de ser updated com valores
    Torneio não tem plots por isso ainda não funciona a mudar depois no ficheiro!

--- TrabPL/Ficheiros_Matlab/Fiheiros_Codigo/Exercicio1App_exported.m
+++ TrabPL/Ficheiros_Matlab/Fiheiros_Codigo/Exercicio1App_exported.m
@@ -2,17 +2,21 @@ classdef Exercicio1App_exported < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure                     matlab.ui.Figure
-        GridLayout                   matlab.ui.container.GridLayout
-        SairButton                   matlab.ui.control.Button
-        ValorDropDown                matlab.ui.control.DropDown
-        ValorLabel                   matlab.ui.control.Label
-        GrficosLabel                 matlab.ui.control.Label
-        VoltaraoMenuPrincipalButton  matlab.ui.control.Button
-        ValorButton                  matlab.ui.control.Button
-        ToolboxButton                matlab.ui.control.Button
-        OtimizaodoSenocomGALabel     matlab.ui.control.Label
+        UIFigure                 matlab.ui.Figure
+        GridLayout               matlab.ui.container.GridLayout
+        SairButton               matlab.ui.control.Button
+        EscolhadevaloresMnimoMximoLabel  matlab.ui.control.Label
+        GrficosrepresentadosToolboxValorLabel  matlab.ui.control.Label
+        EX1Exerccio1dafichaqueencontraromnimomximodafunosenousandoalgoritmosgenticosGAProgramadeoptimizacaoXGAFITNESSFCNNVARSAbAeqbeqlbubNONLCONoptionsLabel  matlab.ui.control.Label
+        ValorDropDown            matlab.ui.control.DropDown
+        Label                    matlab.ui.control.Label
+        MENUPRINCIPALButton      matlab.ui.control.Button
+        SenoButton               matlab.ui.control.Button
+        ToolboxButton            matlab.ui.control.Button
+        OPTIMIZAOSENOCOMGALabel  matlab.ui.control.Label
     end
+
+
 
     % Callbacks that handle component events
     methods (Access = private)
@@ -26,17 +30,18 @@ classdef Exercicio1App_exported < matlab.apps.AppBase
             else
                 run('gasenoMax.m');   % executa o GA do máximo
             end
+        
         end
 
-        % Button pushed function: VoltaraoMenuPrincipalButton
-        function VoltaraoMenuPrincipalButtonPushed(app, event)
+        % Button pushed function: MENUPRINCIPALButton
+        function MENUPRINCIPALButtonPushed(app, event)
             MenuAPP;  % volta ao menu principal
             delete(app);
         end
 
         % Button pushed function: SairButton
         function SairButtonPushed(app, event)
-            delete(app); %sai da aplicação
+            delete(app);
         end
     end
 
@@ -48,86 +53,104 @@ classdef Exercicio1App_exported < matlab.apps.AppBase
 
             % Create UIFigure and hide until all components are created
             app.UIFigure = uifigure('Visible', 'off');
-            app.UIFigure.Position = [100 100 640 480];
+            app.UIFigure.Position = [100 100 967 699];
             app.UIFigure.Name = 'MATLAB App';
 
             % Create GridLayout
             app.GridLayout = uigridlayout(app.UIFigure);
-            app.GridLayout.ColumnWidth = {'1x', 154, 62, 44, 65, 100, '1.01x'};
-            app.GridLayout.RowHeight = {64, '3.42x', 24, 20, '2.82x', 31, '1x', 34, '2.45x', 37};
+            app.GridLayout.ColumnWidth = {'1.13x', 179, 150, 183, '1x'};
+            app.GridLayout.RowHeight = {64, '1.3x', 71, '1.11x', 69, 40, '1x'};
 
-            % Create OtimizaodoSenocomGALabel
-            app.OtimizaodoSenocomGALabel = uilabel(app.GridLayout);
-            app.OtimizaodoSenocomGALabel.BackgroundColor = [0 1 0];
-            app.OtimizaodoSenocomGALabel.HorizontalAlignment = 'center';
-            app.OtimizaodoSenocomGALabel.FontSize = 30;
-            app.OtimizaodoSenocomGALabel.FontWeight = 'bold';
-            app.OtimizaodoSenocomGALabel.Layout.Row = 1;
-            app.OtimizaodoSenocomGALabel.Layout.Column = [2 6];
-            app.OtimizaodoSenocomGALabel.Text = 'Otimização do Seno com GA';
+            % Create OPTIMIZAOSENOCOMGALabel
+            app.OPTIMIZAOSENOCOMGALabel = uilabel(app.GridLayout);
+            app.OPTIMIZAOSENOCOMGALabel.BackgroundColor = [0 1 0];
+            app.OPTIMIZAOSENOCOMGALabel.HorizontalAlignment = 'center';
+            app.OPTIMIZAOSENOCOMGALabel.FontSize = 30;
+            app.OPTIMIZAOSENOCOMGALabel.FontWeight = 'bold';
+            app.OPTIMIZAOSENOCOMGALabel.Layout.Row = 1;
+            app.OPTIMIZAOSENOCOMGALabel.Layout.Column = [1 5];
+            app.OPTIMIZAOSENOCOMGALabel.Text = 'OPTIMIZAÇÃO SENO COM GA';
 
             % Create ToolboxButton
             app.ToolboxButton = uibutton(app.GridLayout, 'push');
             app.ToolboxButton.ButtonPushedFcn = createCallbackFcn(app, @ToolboxButtonPushed, true);
-            app.ToolboxButton.BackgroundColor = [0.651 0.651 0.651];
+            app.ToolboxButton.BackgroundColor = [0.149 0.549 0.8667];
             app.ToolboxButton.FontSize = 18;
+            app.ToolboxButton.FontWeight = 'bold';
             app.ToolboxButton.Layout.Row = 5;
-            app.ToolboxButton.Layout.Column = 2;
+            app.ToolboxButton.Layout.Column = 4;
             app.ToolboxButton.Text = 'Toolbox';
 
-            % Create ValorButton
-            app.ValorButton = uibutton(app.GridLayout, 'push');
-            app.ValorButton.BackgroundColor = [0.651 0.651 0.651];
-            app.ValorButton.FontSize = 18;
-            app.ValorButton.Layout.Row = 6;
-            app.ValorButton.Layout.Column = 2;
-            app.ValorButton.Text = 'Valor';
+            % Create SenoButton
+            app.SenoButton = uibutton(app.GridLayout, 'push');
+            app.SenoButton.BackgroundColor = [0.149 0.549 0.8667];
+            app.SenoButton.FontSize = 18;
+            app.SenoButton.FontWeight = 'bold';
+            app.SenoButton.Layout.Row = 5;
+            app.SenoButton.Layout.Column = 5;
+            app.SenoButton.Text = 'Seno';
 
-            % Create VoltaraoMenuPrincipalButton
-            app.VoltaraoMenuPrincipalButton = uibutton(app.GridLayout, 'push');
-            app.VoltaraoMenuPrincipalButton.ButtonPushedFcn = createCallbackFcn(app, @VoltaraoMenuPrincipalButtonPushed, true);
-            app.VoltaraoMenuPrincipalButton.BackgroundColor = [0.8 0.8 0.8];
-            app.VoltaraoMenuPrincipalButton.FontWeight = 'bold';
-            app.VoltaraoMenuPrincipalButton.Layout.Row = 10;
-            app.VoltaraoMenuPrincipalButton.Layout.Column = 2;
-            app.VoltaraoMenuPrincipalButton.Text = 'Voltar ao Menu Principal';
+            % Create MENUPRINCIPALButton
+            app.MENUPRINCIPALButton = uibutton(app.GridLayout, 'push');
+            app.MENUPRINCIPALButton.ButtonPushedFcn = createCallbackFcn(app, @MENUPRINCIPALButtonPushed, true);
+            app.MENUPRINCIPALButton.BackgroundColor = [0.8 0.8 0.8];
+            app.MENUPRINCIPALButton.FontSize = 18;
+            app.MENUPRINCIPALButton.FontWeight = 'bold';
+            app.MENUPRINCIPALButton.Layout.Row = 2;
+            app.MENUPRINCIPALButton.Layout.Column = 1;
+            app.MENUPRINCIPALButton.Text = 'MENU PRINCIPAL';
 
-            % Create GrficosLabel
-            app.GrficosLabel = uilabel(app.GridLayout);
-            app.GrficosLabel.FontSize = 14;
-            app.GrficosLabel.FontWeight = 'bold';
-            app.GrficosLabel.FontAngle = 'italic';
-            app.GrficosLabel.Layout.Row = 5;
-            app.GrficosLabel.Layout.Column = 1;
-            app.GrficosLabel.Text = 'Gráficos:';
-
-            % Create ValorLabel
-            app.ValorLabel = uilabel(app.GridLayout);
-            app.ValorLabel.HorizontalAlignment = 'center';
-            app.ValorLabel.FontSize = 14;
-            app.ValorLabel.FontWeight = 'bold';
-            app.ValorLabel.FontAngle = 'italic';
-            app.ValorLabel.Layout.Row = 2;
-            app.ValorLabel.Layout.Column = 1;
-            app.ValorLabel.Text = 'Valor:';
+            % Create Label
+            app.Label = uilabel(app.GridLayout);
+            app.Label.FontSize = 14;
+            app.Label.FontWeight = 'bold';
+            app.Label.FontAngle = 'italic';
+            app.Label.Layout.Row = 3;
+            app.Label.Layout.Column = 5;
+            app.Label.Text = '';
 
             % Create ValorDropDown
             app.ValorDropDown = uidropdown(app.GridLayout);
             app.ValorDropDown.Items = {'Mínimo', 'Máximo'};
             app.ValorDropDown.FontSize = 18;
             app.ValorDropDown.FontWeight = 'bold';
-            app.ValorDropDown.BackgroundColor = [0.902 0.902 0.902];
-            app.ValorDropDown.Layout.Row = 2;
+            app.ValorDropDown.BackgroundColor = [0.149 0.549 0.8667];
+            app.ValorDropDown.Layout.Row = 5;
             app.ValorDropDown.Layout.Column = 2;
             app.ValorDropDown.Value = 'Mínimo';
+
+            % Create EX1Exerccio1dafichaqueencontraromnimomximodafunosenousandoalgoritmosgenticosGAProgramadeoptimizacaoXGAFITNESSFCNNVARSAbAeqbeqlbubNONLCONoptionsLabel
+            app.EX1Exerccio1dafichaqueencontraromnimomximodafunosenousandoalgoritmosgenticosGAProgramadeoptimizacaoXGAFITNESSFCNNVARSAbAeqbeqlbubNONLCONoptionsLabel = uilabel(app.GridLayout);
+            app.EX1Exerccio1dafichaqueencontraromnimomximodafunosenousandoalgoritmosgenticosGAProgramadeoptimizacaoXGAFITNESSFCNNVARSAbAeqbeqlbubNONLCONoptionsLabel.HorizontalAlignment = 'center';
+            app.EX1Exerccio1dafichaqueencontraromnimomximodafunosenousandoalgoritmosgenticosGAProgramadeoptimizacaoXGAFITNESSFCNNVARSAbAeqbeqlbubNONLCONoptionsLabel.WordWrap = 'on';
+            app.EX1Exerccio1dafichaqueencontraromnimomximodafunosenousandoalgoritmosgenticosGAProgramadeoptimizacaoXGAFITNESSFCNNVARSAbAeqbeqlbubNONLCONoptionsLabel.Layout.Row = 2;
+            app.EX1Exerccio1dafichaqueencontraromnimomximodafunosenousandoalgoritmosgenticosGAProgramadeoptimizacaoXGAFITNESSFCNNVARSAbAeqbeqlbubNONLCONoptionsLabel.Layout.Column = [2 5];
+            app.EX1Exerccio1dafichaqueencontraromnimomximodafunosenousandoalgoritmosgenticosGAProgramadeoptimizacaoXGAFITNESSFCNNVARSAbAeqbeqlbubNONLCONoptionsLabel.Text = {'EX1:'; 'Exercício 1 da ficha, que é encontrar o mínimo/máximo da função seno usando algoritmos genéticos (GA)'; ''; '%Programa de optimizacao'; '%X = GA(FITNESSFCN,NVARS,A,b,Aeq,beq,lb,ub,NONLCON,options)'};
+
+            % Create GrficosrepresentadosToolboxValorLabel
+            app.GrficosrepresentadosToolboxValorLabel = uilabel(app.GridLayout);
+            app.GrficosrepresentadosToolboxValorLabel.HorizontalAlignment = 'center';
+            app.GrficosrepresentadosToolboxValorLabel.WordWrap = 'on';
+            app.GrficosrepresentadosToolboxValorLabel.Layout.Row = 4;
+            app.GrficosrepresentadosToolboxValorLabel.Layout.Column = [4 5];
+            app.GrficosrepresentadosToolboxValorLabel.Text = {'Gráficos representados!'; 'Toolbox;'; 'Valor;'};
+
+            % Create EscolhadevaloresMnimoMximoLabel
+            app.EscolhadevaloresMnimoMximoLabel = uilabel(app.GridLayout);
+            app.EscolhadevaloresMnimoMximoLabel.HorizontalAlignment = 'center';
+            app.EscolhadevaloresMnimoMximoLabel.WordWrap = 'on';
+            app.EscolhadevaloresMnimoMximoLabel.Layout.Row = 4;
+            app.EscolhadevaloresMnimoMximoLabel.Layout.Column = 2;
+            app.EscolhadevaloresMnimoMximoLabel.Text = {'Escolha de valores:'; 'Mínimo:'; 'Máximo:'};
 
             % Create SairButton
             app.SairButton = uibutton(app.GridLayout, 'push');
             app.SairButton.ButtonPushedFcn = createCallbackFcn(app, @SairButtonPushed, true);
             app.SairButton.BackgroundColor = [0.8 0.8 0.8];
+            app.SairButton.FontSize = 24;
             app.SairButton.FontWeight = 'bold';
-            app.SairButton.Layout.Row = 10;
-            app.SairButton.Layout.Column = 6;
+            app.SairButton.Layout.Row = 7;
+            app.SairButton.Layout.Column = 1;
             app.SairButton.Text = 'Sair';
 
             % Show the figure after all components are created
